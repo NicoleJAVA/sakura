cd2769c0c8fc5546c8d79313c02da24118ff0254
Nicole Chang <powernicole2@yahoo.com.tw>
2021-08-08 17:11:34 +0800

    refactor: refactor code format of all .vue files using prettier

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="container mt-5">
-    <form class="row justify-content-center"
-      @submit.prevent="signIn">
+    <form class="row justify-content-center" @submit.prevent="signIn">
       <div class="col-md-6">
         <h1 class="h3 mb-3 font-weight-normal">請先登入</h1>
         <div class="mb-2">
@@ -29,7 +28,9 @@
         </div>
 
         <div class="text-end mt-4">
-          <button class="btn btn-lg btn-primary btn-block" type="submit">登入</button>
+          <button class="btn btn-lg btn-primary btn-block" type="submit">
+            登入
+          </button>
         </div>
       </div>
     </form>
@@ -44,7 +45,7 @@ export default {
         username: '',
         password: '',
       },
-    };
+    }
   },
 }
 </script>
