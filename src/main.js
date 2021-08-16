8b56d6b4fe2f7742db6d60ba1f11169d40b657d7
Nicole Chang <powernicole2@yahoo.com.tw>
2021-08-16 23:51:58 +0800

    package: import vee-validate

--- src/main.js
+++ src/main.js
@@ -8,18 +8,36 @@ import 'vue3-loading-overlay/dist/vue3-loading-overlay.css';
 import pushMessage from './methods/pushMessage';
 import { currency, date } from './methods/filters';
 import isApiSuccess from './methods/isApiSuccess';
-import 'bootstrap-icons/font/bootstrap-icons.css'
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import { Form, Field, ErrorMessage, defineRule, configure } from 'vee-validate';
+import AllRules from '@vee-validate/rules';
+import { localize, setLocale } from '@vee-validate/i18n';
+import zhTW from '@vee-validate/i18n/dist/locale/zh_TW.json';
 
 const app = createApp(App);
 app.config.globalProperties.$filters = {
   currency,
   date,
 };
+
+Object.keys(AllRules).forEach(rule => {
+  defineRule(rule, AllRules[rule]);
+});
+
+configure({
+  generateMessage: localize({ zh_TW: zhTW }),
+  validateOnInput: true,
+});
+setLocale('zh_TW');
+
 app.config.globalProperties.$api =
   process.env.VUE_APP_API + '/api/' + process.env.VUE_APP_PATH;
 app.use(router);
 app.use(VueAxios, axios);
 app.component('Loading', Loading);
+app.component('Form', Form);
+app.component('Field', Field);
+app.component('ErrorMessage', ErrorMessage);
 app.provide('pushMessage', pushMessage);
 app.provide('isApiSuccess', isApiSuccess);
 app.mount('#app');
