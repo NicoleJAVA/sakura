167f7639196555e50625e14b795e9cfdaea81440
Nicole Chang <powernicole2@yahoo.com.tw>
2021-09-21 17:39:11 +0800

    feat(Slider): auto slide; prev/next arrows

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,96 +1,37 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br />
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
-        >vue-cli documentation</a
-      >.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
+  <div id="slider">
+    <ul class="slide-container">
       <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel"
-          target="_blank"
-          rel="noopener"
-          >babel</a
-        >
+        <a href="#"><img src="../assets/img/slider/slider_1.png"/></a>
       </li>
       <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router"
-          target="_blank"
-          rel="noopener"
-          >router</a
-        >
+        <a href="#"><img src="../assets/img/slider/slider_2.png"/></a>
       </li>
       <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint"
-          target="_blank"
-          rel="noopener"
-          >eslint</a
-        >
+        <a href="#"><img src="../assets/img/slider/slider_3.png"/></a>
+      </li>
+      <li>
+        <a href="#"><img src="../assets/img/slider/slider_4.png"/></a>
+      </li>
+      <li>
+        <a href="#"><img src="../assets/img/slider/slider_5.png"/></a>
       </li>
     </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li>
-        <a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a>
-      </li>
-      <li>
-        <a href="https://forum.vuejs.org" target="_blank" rel="noopener"
-          >Forum</a
-        >
-      </li>
-      <li>
-        <a href="https://chat.vuejs.org" target="_blank" rel="noopener"
-          >Community Chat</a
-        >
-      </li>
-      <li>
-        <a href="https://twitter.com/vuejs" target="_blank" rel="noopener"
-          >Twitter</a
-        >
-      </li>
-      <li>
-        <a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a>
-      </li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li>
-        <a href="https://router.vuejs.org" target="_blank" rel="noopener"
-          >vue-router</a
-        >
-      </li>
-      <li>
-        <a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-devtools#vue-devtools"
-          target="_blank"
-          rel="noopener"
-          >vue-devtools</a
-        >
-      </li>
-      <li>
-        <a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener"
-          >vue-loader</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-          rel="noopener"
-          >awesome-vue</a
-        >
-      </li>
-    </ul>
+    <div class="arrow-container">
+      <span class="prev-arrow">
+        <i class="bi bi-arrow-left-circle"></i>
+      </span>
+      <span class="next-arrow">
+        <i class="bi bi-arrow-right-circle"></i>
+      </span>
+    </div>
+    <div class="indicator-container">
+      <span class="indicator active"></span>
+      <span class="indicator"></span>
+      <span class="indicator"></span>
+      <span class="indicator"></span>
+      <span class="indicator"></span>
+    </div>
   </div>
 </template>
 
@@ -100,23 +41,235 @@ export default {
   props: {
     msg: String,
   },
+  data () {
+    return {
+      autoplay: true,
+      autoplayDelay: 2000,
+      autoplayId: 0,
+      intervalId: 0,
+      slider: null,
+      slideContainer: null,
+      slides: null,
+      indicatorContainer: null,
+      slideWidth: 0,
+      curIndex: 0,
+    };
+  },
+  methods: {
+    initElements () {
+      this.slider = document.getElementById('slider');
+      this.slides = this.slider.getElementsByTagName('li');
+      this.slideContainer = this.slider.getElementsByClassName(
+        'slide-container'
+      )[0];
+      this.indicatorContainer = this.slider.getElementsByClassName(
+        'indicator-container'
+      )[0];
+
+      const firstSlide = this.slides[0].cloneNode(true);
+      this.slideContainer.appendChild(firstSlide);
+      this.indicators = this.indicatorContainer.children;
+
+      this.slideWidth = this.slides[0].offsetWidth;
+    },
+    initEvents () {
+      const that = this;
+      this.slider.addEventListener('mouseover', function () {
+        clearTimeout(that.autoplayId);
+        that.autoplay = false;
+      });
+      this.slider.addEventListener('mouseout', function () {
+        that.autoplay = true;
+        that.startAnimation(that.slideContainer);
+      });
+
+      for (let i = 0; i < this.indicators.length; i++) {
+        this.indicators[i].setAttribute('index', i);
+        this.indicators[i].addEventListener('click', function () {
+          const index = parseInt(this.getAttribute('index'));
+          this.onIndicatorClicked(index);
+        });
+      }
+
+      const prevArrow = this.slider.getElementsByClassName('prev-arrow')[0];
+      const nextArrow = this.slider.getElementsByClassName('next-arrow')[0];
+      prevArrow.addEventListener('click', function () {
+        that.onPrevArrowClicked();
+      });
+      nextArrow.addEventListener('click', function () {
+        that.onNextArrowClicked();
+      });
+    },
+    animate (destinationX) {
+      let speed = 10;
+      let time = 10;
+      const step =
+        (destinationX - this.slideContainer.offsetLeft) / this.slideWidth;
+
+      speed = speed * step;
+      time = time / Math.abs(step);
+
+      clearInterval(this.intervalId);
+      this.setCurrentActiveIndicator(this.curIndex);
+
+      this.intervalId = setInterval(() => {
+        const isLeft = destinationX > this.slideContainer.offsetLeft + speed;
+
+        if ((speed < 0 && isLeft) || (speed > 0 && !isLeft)) {
+          clearInterval(this.intervalId);
+          this.intervalId = -1;
+          this.slideContainer.style.left = destinationX + 'px';
+
+          if (this.autoplay) {
+            this.startAnimation(this.slideContainer);
+          }
+
+          return;
+        }
+
+        this.slideContainer.style.left =
+          this.slideContainer.offsetLeft + speed + 'px';
+      }, time);
+    },
+    onPrevArrowClicked () {
+      const li = this.slideContainer.children;
+      this.curIndex = this.curIndex - 1;
+      if (this.curIndex < 0) {
+        this.slideContainer.style.left =
+          -((li.length - 1) * this.slideWidth) + 'px';
+        this.curIndex = li.length - 2;
+      }
+      this.animate(-(this.curIndex * this.slideWidth));
+    },
+    onNextArrowClicked (nextIndex) {
+      const li = this.slideContainer.children;
+
+      this.curIndex = this.curIndex + 1;
+
+      if (this.curIndex > li.length - 1) {
+        this.slideContainer.style.left = 0 + 'px';
+        this.curIndex = 1;
+      }
+
+      this.animate(-(this.curIndex * this.slideWidth));
+    },
+    setCurrentActiveIndicator () {},
+    startAnimation () {
+      this.autoplayId = setTimeout(() => {
+        this.onNextArrowClicked();
+      }, this.autoplayDelay);
+    },
+  },
+  mounted () {
+    this.initElements();
+    this.initEvents();
+    if (this.autoplay) {
+      this.startAnimation();
+    }
+  },
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
+* {
+  margin: 0;
   padding: 0;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+
+#slider,
+.slide-container li ul,
+.slide-container li a {
+  width: 600px;
+  height: 400px;
 }
-a {
-  color: #42b983;
+
+#slider {
+  position: absolute;
+  /*padding: 3px;*/
+  margin: 10px 0 0 10px;
+  overflow: hidden;
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+}
+
+.slide-container {
+  position: absolute;
+  list-style-type: none;
+  width: 6000px;
+  /*transition-duration: 0.3s;*/
+}
+
+.slide-container li {
+  float: left;
+}
+
+.slide-container li a {
+  display: inline-block;
+}
+
+.slide-container li a img {
+  width: 100%;
+  height: 100%;
+}
+
+#slider:hover .arrow-container {
+  display: block;
+}
+.arrow-container {
+  position: absolute;
+  width: 100%;
+  height: 50px;
+  top: 50%;
+  margin-top: -25px;
+  display: none;
+}
+
+.arrow-container span {
+  position: absolute;
+  width: 50px;
+  height: 50px;
+  font-size: 40px;
+  /*display: inline-block;*/
+  text-align: center;
+  line-height: 50px;
+  background: rgba(0, 0, 0, 0.2);
+  color: white;
+}
+
+.prev-arrow {
+  left: 0px;
+  cursor: pointer;
+}
+
+.next-arrow {
+  right: 0px;
+  cursor: pointer;
+}
+
+.indicator-container {
+  position: absolute;
+  width: 150px;
+  height: 30px;
+  line-height: 30px;
+  bottom: 0;
+  left: 50%;
+  margin-left: -75px;
+  text-align: center;
+}
+
+.indicator {
+  display: inline-block;
+  width: 15px;
+  height: 15px;
+  border-radius: 100%;
+  background: white;
+  cursor: pointer;
+}
+
+.indicator.active {
+  background: red;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,23 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="d-flex flex-column align-items-center" style="height: max-content;">
+    <Navbar></Navbar>
+    <router-view />
+    <div class="slider-wrapper d-flex position-relative">
+      <HelloWorld/>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue';
+import Navbar from '../components/Navbar.vue';
 
 export default {
   name: 'Home',
   components: {
     HelloWorld,
+    Navbar,
   },
 };
 </script>
