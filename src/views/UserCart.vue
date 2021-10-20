ffe0f913fe80cb82bdc13032b4746200a8592ece
Nicole Chang <powernicole2@yahoo.com.tw>
2021-10-20 18:24:41 +0800

    feat(UserCart): increase summary section width; decrease detail section width

--- src/views/UserCart.vue
+++ src/views/UserCart.vue
@@ -2,101 +2,137 @@
   <Loading :active="isLoading"></Loading>
   <div class="container container-custom-width">
     <div class="row mt-4">
-      <ul class="col-md-12 col-lg-8">
-        <li
-          v-for="item in cart.carts"
-          :key="item.id"
-          class="row align-items-center my-3"
-        >
-          <!-- column of product image -->
-          <div class="col">
-            <div class="ratio-wrap" style="--w: 276; --h: 230;">
-              <img
-                class="ratio"
-                alt="image of the product"
-                :src="item.product.imageUrl"
-              />
-            </div>
-          </div>
-
-          <!-- column of product title -->
-          <div class="col">
-            <div class="break-line-10">
+      <ul class="col-md-12 col-lg-7">
+        <li v-for="item in cart.carts" class="mb-4" :key="item.id">
+          <!-- product title -->
+          <div>
+            <div class="break-line-10 text-primary">
               {{ item.product.title }}
-              <div class="text-info" v-if="item.coupon">已套用優惠券</div>
             </div>
           </div>
 
-          <!-- column of product price -->
-          <div class="col">
-            <div
-              v-if="item.product.price !== item.product.origin_price"
-              class="text-decoration-line-through text-deleted
-              fs-6 fw-300 text-center"
-            >
-              NTD $ {{ $filters.currency(item.product.origin_price) }}
-            </div>
-            <div class="fs-6 fw-300 text-subtitle text-center">
-              <span class="text-primary">NTD</span> $
-              {{ $filters.currency(item.product.price) }}
-            </div>
-          </div>
-
-          <!-- column of quantity -->
-          <div class="col d-flex align-items-center">
-            <div class="cart-item-num text-center">{{ item.qty }}</div>
-
-            <div class="d-flex">
-              <button
-                class="left-rounded fs-4 fw-light btn btn-outline-primary py-0 btn-link"
-                :disabled="item.qty <= 1"
-                @click="updateCart(item, item.qty - 1)"
-              >
-                <i class="bi bi-dash"></i>
-              </button>
-              <button
-                class="right-rounded fs-4 fw-light btn btn-outline-primary py-0 btn-link"
-                @click="updateCart(item, item.qty + 1)"
-              >
-                <i class="bi bi-plus"></i>
-              </button>
-              <button
-                type="button"
-                class="btn btn-link fs-5 ms-3"
-                :disabled="status.loadingItem === item.id"
-                @click="removeCartItem(item.id)"
-              >
-                <i class="bi bi-trash"></i>
-              </button>
-            </div>
-          </div>
-
-          <!-- column of subtotal -->
-          <div class="col">
-            <div class="fs-6 fw-300 text-subtitle text-center">
-              <span class="text-primary">NTD</span> $
-              {{ $filters.currency(item.total) }}
-            </div>
-            <div class="text-center">
-              <div v-if="item.final_total !== item.total" class="text-info">
-                折扣價：
+          <!-- container of cart detail columns  -->
+          <div class="row align-items-center">
+            <!-- column of product image -->
+            <div class="col">
+              <div class="ratio-wrap" style="--w: 276; --h: 230;">
+                <img
+                  class="ratio"
+                  alt="image of the product"
+                  :src="item.product.imageUrl"
+                />
               </div>
+            </div>
+
+            <!-- column of product price -->
+            <div class="col">
+              <div
+                v-if="item.product.price !== item.product.origin_price"
+                class="fs-6 fw-300 text-center"
+              >
+                <div class="d-inline-block label-2 py-1 me-2 mb-3 text-primary">
+                  原價
+                </div>
+                <div
+                  class="d-inline-block label-4
+                    text-decoration-line-through text-deleted"
+                >
+                  $ {{ $filters.currency(item.product.origin_price) }}
+                </div>
+              </div>
+
               <div class="fs-6 fw-300 text-subtitle text-center">
-                <span class="text-primary">NTD</span> $
-                {{ $filters.currency(item.final_total) }}
+                <div
+                  class="d-inline-block label-2 py-1 me-2 mb-3
+                border border-primary text-primary"
+                >
+                  特價
+                </div>
+                <div class="d-inline-block label-4">
+                  $ {{ $filters.currency(item.product.price) }}
+                </div>
+              </div>
+            </div>
+
+            <!-- column of quantity -->
+            <div class="col">
+              <div class="row">
+                <div
+                  class="col flex-shrink-1 quantity-wrap
+                    d-flex flex-column align-items-center"
+                >
+                  <div class="cart-item-num text-center mb-3">
+                    {{ item.qty }}
+                  </div>
+
+                  <span class="mx-auto d-flex flex-nowrap">
+                    <button
+                      class="left-rounded fs-6 fw-light line-height-1
+                        btn btn-outline-primary py-0 px-0 btn-link"
+                      :disabled="item.qty <= 1"
+                      @click="updateCart(item, item.qty - 1)"
+                    >
+                      <i class="bi bi-dash"></i>
+                    </button>
+                    <button
+                      class="right-rounded fs-6 fw-light line-height-1
+                      btn btn-outline-primary py-0 px-0 btn-link"
+                      @click="updateCart(item, item.qty + 1)"
+                    >
+                      <i class="bi bi-plus"></i>
+                    </button>
+                  </span>
+                </div>
+
+                <div class="col d-flex justify-content-center">
+                  <button
+                    type="button"
+                    class="btn btn-link fs-5"
+                    :disabled="status.loadingItem === item.id"
+                    @click="removeCartItem(item.id)"
+                  >
+                    <i class="bi bi-trash"></i>
+                  </button>
+                </div>
+              </div>
+            </div>
+            <!-- column of subtotal -->
+            <div class="col">
+              <div class="fs-6 fw-300 text-subtitle text-center">
+                <div
+                  class="d-inline-block label-3 py-1 me-2 mb-3
+                border border-primary text-primary"
+                >
+                  小計
+                </div>
+                <div class="d-inline-block label-4">
+                  $ {{ $filters.currency(item.total) }}
+                </div>
+              </div>
+
+              <div class="fs-6 fw-300 text-subtitle text-center">
+                <div
+                  class="d-inline-block label-3 py-1 me-2 mb-2
+                bg-primary text-white"
+                >
+                  折扣價
+                </div>
+                <div class="d-inline-block label-4">
+                  $ {{ $filters.currency(item.final_total) }}
+                </div>
               </div>
             </div>
           </div>
         </li>
       </ul>
 
-      <div class="col-md-12 col-lg-4">
+      <div class="col-md-12 col-lg-5">
         <div class="bg-section h-100 ms-5 pt-6">
           <GappedDialog class="mx-auto mb-6" />
 
           <div class="row mb-4">
             <div class="col-1"></div>
-            <div class="col-4 d-flex">
+            <div class="col-5 d-flex">
               <div class="text-white bg-label label-4 py-2 px-2">總計</div>
             </div>
             <div
@@ -109,7 +145,7 @@
 
           <div class="row mb-4">
             <div class="col-1"></div>
-            <div class="col-4 d-flex">
+            <div class="col-5 d-flex">
               <div class="text-white bg-label label-4 py-2 px-2">折扣價</div>
             </div>
             <div
@@ -122,18 +158,18 @@
 
           <div class="row mb-5">
             <div class="col-1"></div>
-            <div class="col-4">
+            <div class="col-7 col-lg-5">
               <input
                 type="text"
-                class="border border-primary rounded-6 input-5 py-2 px-2 text-secondary"
+                class="border border-primary rounded-6 w-100 py-2 px-2 text-secondary"
                 v-model="coupon_code"
                 placeholder="請輸入優惠碼"
               />
             </div>
 
-            <div class="col-5 d-flex flex-row-reverse">
+            <div class="col-3 col-lg-5 d-flex flex-row-reverse">
               <button
-                class="btn btn-secondary text-white"
+                class="btn btn-secondary text-white w-100"
                 type="button"
                 @click="addCouponCode"
               >
@@ -324,6 +360,22 @@
     </div>
   </div>
 </template>
+
+<style scoped lang="scss">
+$quantity-btn-height: 30px;
+$quantity-btn-width: 30px;
+$quantity-wrap-width: 130px;
+
+.left-rounded,
+.right-rounded {
+  height: $quantity-btn-height;
+  width: $quantity-btn-width;
+}
+
+.quantity-wrap {
+  width: $quantity-wrap-width;
+}
+</style>
 
 <script>
 import GappedDialog from '@/components/GappedDialog.vue';
