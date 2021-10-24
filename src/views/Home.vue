1a505299593d83eb59b202e8a25514debb2c065d
Nicole Chang <powernicole2@yahoo.com.tw>
2021-10-24 18:29:14 +0800

    feat(home): layout of street, yaki booth, and okashi booth

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,42 +1,78 @@
 <template>
-  <div class="d-flex justify-content-center primary-background">
-    <div class="position-absolute">
-      <div class="fade-anim-wrap">
-        <FadeAnim id="home-fade-anim" />
+  <div class="primary-background">
+    <!-- street -->
+    <div id="street" class="position-relative mx-auto">
+      <div id="sakura-cafe" class="mx-auto">
+        <img
+          src="../assets/img/home/sakura_cafe.png"
+          alt="cafe"
+          class="w-100"
+        />
       </div>
+
+      <!-- yaki booth -->
+      <img
+        id="yaki-booth"
+        src="../assets/img/home/sakura_yaki_booth.png"
+        alt="yaki booth"
+        class="position-absolute"
+      />
+
+      <!-- okashi booth -->
+      <img
+        id="okashi-booth"
+        src="../assets/img/home/sakura_okashi_booth.png"
+        alt="okashi booth"
+        class="position-absolute"
+      />
+
+      <!-- menu fader animation -->
+      <FadeAnim id="home-fade-anim" />
     </div>
-    <div class="d-flex flex-column align-items-center">
-      <img id="sakura-cafe" src="../assets/img/home/sakura_cafe.png" alt="cafe" />
-      <router-view />
-      <div class="slider-wrapper d-flex position-relative">
-        <Slider ref="slider" />
-      </div>
-    </div>
+  </div>
+  <div class="slider-wrapper d-flex position-relative">
+    <Slider ref="slider" />
   </div>
 </template>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
+$street-width: 1370px;
+$sakura-cafe-width: 800px;
+$yaki-booth-width: 370px;
+$yaki-booth-left-offset: 0px;
+$yaki-booth-bottom-offset: 0px;
+$okashi-booth-width: 271px;
+$okashi-booth-right-offset: 60px;
+$okashi-booth-bottom-offset: 0px;
+$fade-anim-top-offset: 20px;
+$fade-anim-right-offset: 360px;
 
-$sakura-cafe-width: 1000px;
-$fade-anim-top-offset: 80px;
-$fade-anim-right-offset: 30px;
-$fade-anim-wrap-width: 1000px;
+#street {
+  width: $street-width;
+}
 
 #sakura-cafe {
   width: $sakura-cafe-width;
   height: auto;
 }
 
+#yaki-booth {
+  width: $yaki-booth-width;
+  left: $yaki-booth-left-offset;
+  bottom: $yaki-booth-bottom-offset;
+}
+
+#okashi-booth {
+  width: $okashi-booth-width;
+  right: $okashi-booth-right-offset;
+  bottom: $okashi-booth-bottom-offset;
+}
+
 #home-fade-anim {
   position: absolute;
   top: $fade-anim-top-offset;
   right: $fade-anim-right-offset;
-}
-
-.fade-anim-wrap {
-  width: $fade-anim-wrap-width;
-  position: relative;
 }
 
 </style>
